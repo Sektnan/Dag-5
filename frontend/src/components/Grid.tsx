5fe16b886a7fc35210ff308333dc213951b3c8bf
Sektnan <robinsektnan@hotmail.com>
2024-09-19 14:03:23 +0200

    12. La til AddStudentForm. Proppa til Grid. 13 bruk AddStudentForm i grid og test. La også med onAddStudent funksjon

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Student from "./Student";
 import type { Student as StudentProp } from "./types";
+import AddStudentForm from "./AddStudentForm";
 
 type GridProps = {
     students: StudentProp[];
@@ -8,13 +9,20 @@ type GridProps = {
 
 export default function Grid(props: GridProps) {
     // props.students ?? []
-    const [students, setStudents] = useState(props.students ?? []);
+    const [students, setStudents] = useState<StudentProp[]>(props.students ?? []);
+
+    const onAddStudent = (student: { name: string }) => {
+        setStudents((prev) => [...prev, { id: crypto.randomUUID(), ...student }]);
+    };
 
     return (
-        <article className="grid">
-            {students.map((student) => (
-                <Student key={student.id} name={student.name} id={student.id} />
-            ))}
-        </article>
+        <section>
+            <article className="grid">
+                {students.map((student) => (
+                    <Student key={student.id} name={student.name} id={student.id} />
+                ))}
+            </article>
+            <AddStudentForm onAddStudent={onAddStudent} />
+        </section>
     );
 }
